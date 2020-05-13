e054990a349d4d43bf5807c30992246748c0518c
Jacob Lapworth <jacob@lapworth.nz>
2020-05-13 17:20:08 +1200

    ✨ Init

---
+++ api/index.js
@@ -0,0 +1,33 @@
+import readyForPayment from './workflows/readyForPayment'
+
+async function workflow(event) {
+  console.log('Event:', event);
+
+  const events = {
+    'sale.ready_for_payment': readyForPayment,
+  };
+
+  if (typeof events[event.event_type] === 'undefined') {
+    return null;
+  }
+
+  return events[event.event_type](event);
+}
+
+export default async (req, res) => {
+  if (req.body == null) {
+    return res.status(400).send({ error: 'No payload body.' })
+  }
+
+  try {
+    await workflow(req.body).then((actions) => {
+      console.log('Response:', actions);
+      res.status(200).send(actions)
+    });
+
+  } catch (error) {
+    return res.status(400).json({ error: 'Workflow failed.' })
+  }
+
+}
+
