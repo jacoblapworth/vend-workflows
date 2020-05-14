3149b36a4de79b201889c5bf5b20d6eb5bf6c620
Jacob Lapworth <jacob@lapworth.nz>
2020-05-14 20:28:36 +1200

    Dynamic domain_prefix

--- api/vend/callback.js
+++ api/vend/callback.js
@@ -1,17 +1,38 @@
-import oauth2, { config } from '../_utils/oauth'
+import { config } from '../_utils/oauth'
+import simpleOauth from 'simple-oauth2'
 
 export default (req, res) => {
 
   const code = req.query.code
   const domainPrefix = req.query.domain_prefix
 
-  oauth2.authorizationCode.getToken({
+  const tokenConfig = {
     code: code,
     redirect_uri: config.redirect_uri,
+    tokenHost: `https://${domainPrefix}.vendhq.com`,
+    tokenPath: `/api/1.0/token`,
     client_id: config.clientId,
-    client_secret: config.clientSecret
+    client_secret: config.clientSecret,
+  };
+
+  const oauth2 = simpleOauth.create({
+    client: {
+      id: config.clientId,
+      secret: config.clientSecret
+    },
+    auth: {
+      authorizeHost: config.authorizeHost,
+      authorizePath: config.authorizePath,
+      tokenHost: `https://${domainPrefix}.vendhq.com`,
+      tokenPath: config.tokenPath,
+    }
   })
+
+  oauth2.authorizationCode.getToken(tokenConfig)
     .then((result) => {
+
+      console.log("Result:", result);
+
       const token = oauth2.accessToken.create(result)
       console.log('access_token', token.token)
       const accessToken = token.token
