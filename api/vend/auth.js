2dad3e4215f229c87ab76adf0ffe5761d54a21a6
Jacob Lapworth <jacob@lapworth.nz>
2020-05-15 11:24:35 +1200

    Cleanup auth

--- api/vend/auth.js
+++ api/vend/auth.js
@@ -1,28 +1,5 @@
 import oauth2, { config } from '../_utils/oauth'
 
-// /* Do initial auth redirect */
-// exports.handler = (event, context, callback) => {
-//   /* Generate authorizationURI */
-//   const authorizationURI = oauth2.authorizationCode.authorizeURL({
-//     redirect_uri: config.redirect_uri,
-//     /* State helps mitigate CSRF attacks & Restore the previous state of your app */
-//     state: '',
-//   })
-
-//   /* Redirect user to authorizationURI */
-//   const response = {
-//     statusCode: 302,
-//     headers: {
-//       Location: authorizationURI,
-//       'Cache-Control': 'no-cache' // Disable caching of this response
-//     },
-//     body: '' // return body for local dev
-//   }
-
-//   return callback(null, response)
-// }
-
-
 export default (req, res) => {
 
   /* Generate authorizationURI */
