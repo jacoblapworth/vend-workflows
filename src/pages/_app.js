a01db354f42d62423d3b479241431cbf5671e95e
Jacob Lapworth <jacob@lapworth.nz>
2020-06-12 11:16:53 +1200

    Cleanup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,11 @@ import { NavHeader } from '../components/nav/NavHeader';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function Workflows({ Component, pageProps }) {
+
+  if (typeof window === 'undefined') {
+    global.window = {}
+  }
+
   return (
     <>
       <NavHeader />
