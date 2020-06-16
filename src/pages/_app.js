d730957c011956f9db48f5a4f81147b5d4cb2d3c
Jacob Lapworth <jacob@lapworth.nz>
2020-06-16 13:33:38 +1200

    Header

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
+import React, { useEffect } from 'react'
 import cookies from 'next-cookies'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 import '@vendhq/vend-styles/dist/vend-styles.css'
 import { NavHeader } from '../components/nav/NavHeader'
 import { SideBar } from '../components/nav/SideBar'
@@ -7,9 +9,22 @@ import { Body, Content } from '../components/styled'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function Workflows({ Component, pageProps }) {
+  const { token } = pageProps.cookies
   const router = useRouter()
+
+  // useEffect(() => {
+  //   if (!token) router.push('/')
+  // })
+
   return (
     <>
+      <Head>
+        <title>Workflows Demo | Vend</title>
+        <link
+          rel="shortcut icon"
+          href="https://www.vendhq.com/favicon.ico"
+        ></link>
+      </Head>
       <NavHeader />
       <Body className="vd-body">
         <SideBar active={router.pathname} />
