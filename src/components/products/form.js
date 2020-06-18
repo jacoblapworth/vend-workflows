60428c0f1d4446aafcd918166bf1850f3637c9fa
Jacob Lapworth <jacob@lapworth.nz>
2020-06-18 15:37:32 +1200

    Default value

--- src/components/products/form.js
+++ src/components/products/form.js
@@ -1,41 +1,40 @@
 import React, { Fragment } from 'react'
 import { useForm } from 'react-hook-form'
 
-import { Button } from '../SharedReact'
 import { InputField } from '../InputField'
 import { Switch } from '../Switch'
 import { Label } from '../Label'
 
 export const Form = (props) => {
-  const { data, onClose } = props
+  const { data } = props
 
-  const defaultValues = () => {
-    const values = {}
-    data.product.customFields.forEach((customField) => {
-      const type = customField['__typename']
+  // const defaultValues = () => {
+  //   const values = {}
+  //   data.product.customFields.forEach((customField) => {
+  //     const type = customField['__typename']
 
-      let value
-      switch (type) {
-        case 'StringCustomFieldValue':
-          value = customField.stringValue
-          break
-        case 'IntegerCustomFieldValue':
-          value = customField.integerValue
-          break
-        case 'BooleanCustomFieldValue':
-          value = customField.booleanValue ? 'on' : 'off'
-          break
-        default:
-          return null
-      }
+  //     let value
+  //     switch (type) {
+  //       case 'StringCustomFieldValue':
+  //         value = customField.stringValue
+  //         break
+  //       case 'IntegerCustomFieldValue':
+  //         value = customField.integerValue
+  //         break
+  //       case 'BooleanCustomFieldValue':
+  //         value = customField.booleanValue ? 'on' : 'off'
+  //         break
+  //       default:
+  //         return null
+  //     }
 
-      values[customField.name] = value
-    })
-    return values
-  }
+  //     values[customField.name] = value
+  //   })
+  //   return values
+  // }
 
   const { register, errors, handleSubmit } = useForm({
-    defaultValues: defaultValues(),
+    // defaultValues: defaultValues(),
   })
 
   function onSubmit(data) {
@@ -46,6 +45,9 @@ export const Form = (props) => {
     <Fragment>
       <form onSubmit={handleSubmit(onSubmit)} className="vd-mt5 vd-mb5">
         {data.customFields.map((customField) => {
+          const customFieldValue = data.product.customFields.find(
+            (field) => field.name === customField.name
+          )
           switch (customField.type) {
             case 'STRING':
               return (
@@ -55,6 +57,9 @@ export const Form = (props) => {
                   label={customField.title}
                   innerRef={register()}
                   errors={errors}
+                  defaultValue={
+                    customFieldValue && customFieldValue.stringValue
+                  }
                 />
               )
             case 'INTEGER':
@@ -66,12 +71,21 @@ export const Form = (props) => {
                   innerRef={register()}
                   errors={errors}
                   type="number"
+                  defaultValue={
+                    customFieldValue && customFieldValue.integerValue
+                  }
                 />
               )
             case 'BOOLEAN':
               return (
                 <div className="vd-field" key={customField.name}>
-                  <Switch name={customField.name} ref={register} />
+                  <Switch
+                    name={customField.name}
+                    ref={register}
+                    defaultChecked={
+                      customFieldValue && customFieldValue.booleanValue
+                    }
+                  />
                   <Label className="vd-ml2" name={customField.name}>
                     {customField.title}
                   </Label>
@@ -81,18 +95,10 @@ export const Form = (props) => {
               return null
           }
         })}
+        <button onClick={handleSubmit((data) => console.log(data))}>
+          Save
+        </button>
       </form>
     </Fragment>
   )
-}
-
-{
-  /* <div className="vd-dialog-actions">
-  <div className="vd-btn-group">
-    <Button onClick={onClose} variant="supplementary">
-      Cancel
-          </Button>
-    <Button onClick={handleSubmit(onSubmit)}>Save</Button>
-  </div>
-</div> */
 }
