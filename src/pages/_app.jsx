55be15266e4f47b09ca3aec50d6419dee570bb11
Jacob Lapworth <jacob@lapworth.nz>
2020-06-21 17:54:57 +1200

    Merge conflicts

---
+++ src/pages/_app.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react'
+import cookies from 'next-cookies'
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import '@vendhq/vend-styles/dist/vend-styles.css'
+import { NavHeader } from '../components/Nav/NavHeader'
+import { SideBar } from '../components/Nav/SideBar'
+import { DemoWarning } from '../components/DemoWarning'
+import { Body, Content } from '../components/styled'
+import { Providers } from '../components/Providers'
+
+// This default export is required in a new `pages/_app.js` file.
+export default function Workflows({ Component, pageProps }) {
+  const { token } = pageProps.cookies
+  const router = useRouter()
+
+  // useEffect(() => {
+  //   if (!token) router.push('/')
+  // })
+
+  return (
+    <>
+      <Head>
+        <title>Workflows Demo | Vend</title>
+        <link
+          rel="shortcut icon"
+          href="https://www.vendhq.com/favicon.ico"
+        ></link>
+      </Head>
+      <Providers>
+        <NavHeader />
+        <DemoWarning></DemoWarning>
+        <Body className="vd-body">
+          <SideBar active={router.pathname} />
+          <Content className="vd-main-content-container vd-main-content-inner-container">
+            <Component {...pageProps} />
+          </Content>
+        </Body>
+      </Providers>
+    </>
+  )
+}
+
+Workflows.getInitialProps = (context) => {
+  console.log(context)
+  return {
+    pageProps: {
+      cookies: cookies(context.ctx),
+    },
+  }
+}
