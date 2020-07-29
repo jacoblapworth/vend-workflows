824f54f8b4014d54c90fb67382922ab73a2e5c38
Jacob Lapworth <jacob@lapworth.nz>
2020-07-30 10:19:55 +1200

    Internal customer credit

--- src/actions/customerCreditLimit.js
+++ src/actions/customerCreditLimit.js
@@ -1,34 +1,9 @@
 import { WORKFLOW_ACTIONS } from '../constants'
-import { redis } from '../services/redis'
-import axios from 'axios'
 
 export default async function action(lineItem, ctx) {
-  const { domain_prefix } = ctx.retailer
-  const saleCustomer = ctx.customer
+  const { customer } = ctx
 
-  if (!saleCustomer.id) return
-
-  const token = await redis.hgetall(`retailer:${domain_prefix}`)
-  redis.disconnect()
-
-  if (!token) return
-
-  // eslint-disable-next-line no-unused-vars
-  const { access_token, expires, expires_in, refresh_token, expires_at } = token
-
-  const vendApi = axios.create({
-    baseURL: `https://${domain_prefix}.vendhq.com/api`,
-    headers: { Authorization: 'Bearer ' + access_token },
-  })
-
-  const customer = await vendApi
-    .get(`/2.0/customers/${saleCustomer.id}`)
-    .then((res) => {
-      return res.data.data
-    })
-    .catch(console.error)
-
-  console.log('Customer: ', customer)
+  if (!customer.id) return
 
   const action = {
     type: WORKFLOW_ACTIONS.CONFIRM,
